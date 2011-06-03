d579ddf66ee48d3c7c3ce8e1402e3aba04daf118
Witold Rugowski <rugowski@nhw.pl>
2011-06-03 21:57:34 +0200

    First support funcitons for debounce code cleanup

--- NettigoKeypad.cpp
+++ NettigoKeypad.cpp
@@ -2,11 +2,14 @@
 //Published on MIT licence
 // (c) 2011 Nettigo
 
-#include "WProgram.h"
 #include "NettigoKeypad.h"
 
 NG_Keypad::NG_Keypad(void) {
  for (int i=0; i++; i< NG_KEYPAD_SIZE) { _functions[i] = NULL; }
+ //store current time and set last pressed key to none
+ lastKey = NONE;
+ lastKeyTime = millis();
+ debounceDelay = 50;
 };
 
 int NG_Keypad::key_pressed(int rd) {
@@ -18,7 +21,7 @@ int NG_Keypad::key_pressed(int rd) {
   return NONE;
 };
 
-void NG_Keypad::check_handlers(int rd) {
+void NG_Keypad::check_handlers(byte rd) {
   if (_functions[key_pressed(rd)] != NULL) 
   {
     _functions[key_pressed(rd)]();
@@ -27,9 +30,18 @@ void NG_Keypad::check_handlers(int rd) {
 }
 
 
-int NG_Keypad::register_handler( int key, void (*userF)(void) ) {
+int NG_Keypad::register_handler( byte key, void (*userF)(void) ) {
   if(key >= NG_KEYPAD_SIZE)
     return -1;
     _functions[key] = userF;
 };
+
+unsigned int NG_Keypad::getDebounceDelay( void ){
+	return debounceDelay;
+};
+
+void NG_Keypad::setDebounceDelay (unsigned int d){
+	debounceDelay = d;
+};
+
 
--- NettigoKeypad.h
+++ NettigoKeypad.h
@@ -1,7 +1,7 @@
 //Nettigo Keypad library
 //Published on MIT licence
 // (c) 2011 Nettigo
-
+#include "WProgram.h"
 #define NG_KEYPAD_SIZE  6
 
 class NG_Keypad
@@ -14,8 +14,8 @@ class NG_Keypad
   static const byte UP      = 4;
   static const byte RIGHT   = 5;
   
-
   NG_Keypad(void);  
+	
   //which key was pressed? Takes analog input value and returns constant
   //describing which key was pressed.
   int key_pressed(int rd);
@@ -24,13 +24,26 @@ class NG_Keypad
   // takes two args
   // key - which key should be used
   // userF - which function should be called when key pressed
-  int register_handler( int key, void (*userF)(void) );
+  int register_handler( byte key, void (*userF)(void) );
   
-  //Check for handlers to be called
-  void check_handlers(int rd);
-  private:
+  //Check for handlers to be called. Takes as argument analogRead result.
+  void check_handlers(byte rd);
+	
+	//return current debounce delay
+	unsigned int getDebounceDelay( void );
+	
+	//set new debounce delay
+	void setDebounceDelay (unsigned int);
+  
+	private:
   void  (*_functions[NG_KEYPAD_SIZE])(void);
   
+	//default debounce timeout
+	unsigned int debounceDelay;
+	
+	//what last key was pressed and when (millis)
+	byte lastKey;
+	unsigned long lastKeyTime;
   
 };
 
